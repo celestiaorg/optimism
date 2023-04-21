76133bcca3bcb56cc282fa99d31f1ce905c4b882
Javed Khan <tuxcanfly@gmail.com>
2023-04-21 08:45:01 +0530

    da: use celestia as da
    build(deps): update go-da to v0.1.0
    
    build(deps): update go-da to v0.2.0; add ctx
    
    da: add cli flags for da config
    
    da: add get timeout to da client
    
    build(deps): bump local-celestia-devnet to v0.12.7
    
    fix(cli): fix da rpc check
    
    fix(cli): check return err
    
    fix(da): blob data source reuse DataFromEVMTransactions
    
    da: upgrade to go-da v0.5.0
    
    feat(op-batcher): support disable eth fallback
    
    chore(op-batcher): refactor EthFallbackDisabledFlagName
    
    da: use env auth token if set
    
    feat: enable multi frame txs and frame size setting for calldata txs
    
    chore: add log for multiframetxs setting
    
    da: try using blobdata for eth fallback
    
    da: add gas price flag (#451)
    
    da: reuse useblobs for multiframetxs (#452)
    
    Add Github CI (#472)
    
    Cleanup Github CI workflow (#478)
    
    feat: allow to toggle Celestia in op-batcher and op-node (#498)
    
    feat: add op-celestia indexer (#503)
    
    fix: init op-node client
    
    da: use celestia-node client (#504)
    
    da: fix batcher submit (#511)
    
    feat(op-celestia/indexer): add indexed l2 block status (#514)

---
+++ op-celestia/da_client.go
@@ -0,0 +1,62 @@
+package celestia
+
+import (
+	"context"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"time"
+
+	"github.com/celestiaorg/celestia-node/api/rpc/client"
+)
+
+// heightLen is a length (in bytes) of serialized height.
+//
+// This is 8 as uint64 consist of 8 bytes.
+const heightLen = 8
+
+func MakeID(height uint64, commitment []byte) []byte {
+	id := make([]byte, heightLen+len(commitment))
+	binary.LittleEndian.PutUint64(id, height)
+	copy(id[heightLen:], commitment)
+	return id
+}
+
+func SplitID(id []byte) (uint64, []byte) {
+	if len(id) <= heightLen {
+		return 0, nil
+	}
+	commitment := id[heightLen:]
+	return binary.LittleEndian.Uint64(id[:heightLen]), commitment
+}
+
+type DAClient struct {
+	Client        *client.Client
+	GetTimeout    time.Duration
+	SubmitTimeout time.Duration
+	Namespace     []byte
+	FallbackMode  string
+	GasPrice      float64
+}
+
+func NewDAClient(rpc, token, namespace, fallbackMode string, gasPrice float64) (*DAClient, error) {
+	client, err := client.NewClient(context.Background(), rpc, token)
+	if err != nil {
+		return nil, err
+	}
+	ns, err := hex.DecodeString(namespace)
+	if err != nil {
+		return nil, err
+	}
+	if fallbackMode != "disabled" && fallbackMode != "blobdata" && fallbackMode != "calldata" {
+		return nil, fmt.Errorf("celestia: unknown fallback mode: %s", fallbackMode)
+	}
+	return &DAClient{
+		Client:        client,
+		GetTimeout:    time.Minute,
+		SubmitTimeout: time.Minute,
+		Namespace:     ns,
+		FallbackMode:  fallbackMode,
+		GasPrice:      gasPrice,
+	}, nil
+}
