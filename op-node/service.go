fa81f7ad8c99a09a1509c1d909f9ac9271e1c69a
Javed Khan <tuxcanfly@gmail.com>
2023-04-21 08:45:01 +0530

    da: use celestia as da
    build(deps): update go-da to v0.1.0
    
    build(deps): update go-da to v0.2.0; add ctx
    
    da: add cli flags for da config
    
    da: add get timeout to da client
    
    build(deps): bump local-celestia-devnet to v0.12.7
    
    fix(cli): fix da rpc check
    
    fix(cli): check return err
    
    fix(da): blob data source reuse DataFromEVMTransactions
    
    da: upgrade to go-da v0.5.0
    
    feat(op-batcher): support disable eth fallback
    
    chore(op-batcher): refactor EthFallbackDisabledFlagName
    
    da: use env auth token if set
    
    feat: enable multi frame txs and frame size setting for calldata txs
    
    chore: add log for multiframetxs setting
    
    da: try using blobdata for eth fallback
    
    da: add gas price flag (#451)
    
    da: reuse useblobs for multiframetxs (#452)
    
    Add Github CI (#472)
    
    Cleanup Github CI workflow (#478)
    
    feat: allow to toggle Celestia in op-batcher and op-node (#498)
    
    feat: add op-celestia indexer (#503)
    
    fix: init op-node client
    
    da: use celestia-node client (#504)
    
    da: fix batcher submit (#511)
    
    feat(op-celestia/indexer): add indexed l2 block status (#514)

--- op-node/service.go
+++ op-node/service.go
@@ -16,6 +16,7 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 
 	altda "github.com/ethereum-optimism/optimism/op-alt-da"
+	celestia "github.com/ethereum-optimism/optimism/op-celestia"
 	"github.com/ethereum-optimism/optimism/op-node/chaincfg"
 	"github.com/ethereum-optimism/optimism/op-node/config"
 	"github.com/ethereum-optimism/optimism/op-node/flags"
@@ -125,7 +126,8 @@ func NewConfig(ctx *cli.Context, log log.Logger) (*config.Config, error) {
 		},
 		ConductorRpcTimeout: ctx.Duration(flags.ConductorRpcTimeoutFlag.Name),
 
-		AltDA: altda.ReadCLIConfig(ctx),
+		AltDA:    altda.ReadCLIConfig(ctx),
+		DaConfig: celestia.ReadCLIConfig(ctx),
 
 		IgnoreMissingPectraBlobSchedule: ctx.Bool(flags.IgnoreMissingPectraBlobSchedule.Name),
 		FetchWithdrawalRootFromState:    ctx.Bool(flags.FetchWithdrawalRootFromState.Name),
