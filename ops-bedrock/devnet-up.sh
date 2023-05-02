cd0a31af14edde2e9767d4a8d08ddca9d88a9789
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-05-01 21:23:22 -0400

    disable health check and set chmod 777

--- ops-bedrock/devnet-up.sh
+++ ops-bedrock/devnet-up.sh
@@ -50,8 +50,8 @@ export PLATFORM
 # that will store the key for the DA light node
 # to be able to write data to Celestia
 # TODO: determine if this is necessary to include in the script
-sudo chown 10001:10001 $PWD/ops-bedrock/celestia-pwd
-# sudo chmod -R 777 $PWD/ops-bedrock/celestia-pwd
+# sudo chown 10001:10001 $PWD/ops-bedrock/celestia-pwd
+sudo chmod -R 777 $PWD/ops-bedrock/celestia-pwd
 
 L1_URL="http://localhost:8545"
 L2_URL="http://localhost:9545"
