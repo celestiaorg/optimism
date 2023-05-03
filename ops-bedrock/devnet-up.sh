677998fc05d97817524cfdfc7698a3888984edb7
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-05-03 11:59:09 -0400

    remove notes for light node setup

--- ops-bedrock/devnet-up.sh
+++ ops-bedrock/devnet-up.sh
@@ -46,13 +46,6 @@ else
 fi
 export PLATFORM
 
-# Set permissions for the mounted directory
-# that will store the key for the DA light node
-# to be able to write data to Celestia
-# TODO: determine if this is necessary to include in the script
-# sudo chown 10001:10001 $PWD/ops-bedrock/celestia-pwd
-# sudo chmod -R 777 $PWD/ops-bedrock/celestia-pwd
-
 L1_URL="http://localhost:8545"
 L2_URL="http://localhost:9545"
 
