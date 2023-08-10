140734036f350cc7552e0edb0cd543e2510d3b81
Javed Khan <tuxcanfly@gmail.com>
2023-08-10 16:20:32 -0700

    pkg celestia: test - add invalid case

--- op-celestia/celestia/celestia.go
+++ op-celestia/celestia/celestia.go
@@ -3,18 +3,23 @@ package celestia
 import (
 	"encoding"
 	"encoding/binary"
+	"errors"
+)
+
+var (
+	ErrInvalidSize = errors.New("invalid size")
 )
 
 // Framer defines a way to encode/decode a FrameRef.
 type Framer interface {
-  encoding.BinaryMarshaler
-  encoding.BinaryUnmarshaler
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
 }
 
 // FrameRef contains the reference to the specific frame on celestia and
 // satisfies the Framer interface.
 type FrameRef struct {
-	BlockHeight uint64
+	BlockHeight  uint64
 	TxCommitment []byte
 }
 
@@ -22,29 +27,42 @@ var _ Framer = &FrameRef{}
 
 // MarshalBinary encodes the FrameRef to binary
 // serialization format: height + commitment
-//  ----------------------------------------
+//
+//	----------------------------------------
+//
 // | 8 byte uint64  |  32 byte commitment   |
-//  ----------------------------------------
+//
+//	----------------------------------------
+//
 // | <-- height --> | <-- commitment -->    |
-//  ----------------------------------------
-func (f *FrameRef) MarshalBinary() ([]byte, error){
-	ref := make([]byte, 8 + len(f.TxCommitment))
+//
+//	----------------------------------------
+func (f *FrameRef) MarshalBinary() ([]byte, error) {
+	ref := make([]byte, 8+len(f.TxCommitment))
 
 	binary.LittleEndian.PutUint64(ref, f.BlockHeight)
 	copy(ref[8:], f.TxCommitment)
 
-  return ref, nil
+	return ref, nil
 }
 
 // UnmarshalBinary decodes the binary to FrameRef
 // serialization format: height + commitment
-//  ----------------------------------------
+//
+//	----------------------------------------
+//
 // | 8 byte uint64  |  32 byte commitment   |
-//  ----------------------------------------
+//
+//	----------------------------------------
+//
 // | <-- height --> | <-- commitment -->    |
-//  ----------------------------------------
-func (f *FrameRef) UnmarshalBinary(ref []byte) (error){
-  f.BlockHeight = binary.LittleEndian.Uint64(ref[:8])
-  f.TxCommitment = ref[8:]
-  return nil
+//
+//	----------------------------------------
+func (f *FrameRef) UnmarshalBinary(ref []byte) error {
+	if len(ref) <= 8 {
+		return ErrInvalidSize
+	}
+	f.BlockHeight = binary.LittleEndian.Uint64(ref[:8])
+	f.TxCommitment = ref[8:]
+	return nil
 }
--- op-celestia/celestia/celestia_test.go
+++ op-celestia/celestia/celestia_test.go
@@ -9,14 +9,25 @@ import (
 
 func TestEncodeDecodeFrameRef(t *testing.T) {
 	tests := []struct {
-		name string
+		name        string
 		frameRefHex string
-		frameRef FrameRef
+		frameRef    FrameRef
+		isValid     bool
+		err         error
 	}{
 		{
-		"valid frame reference",
-		"d20400000000000068656c6c6f20776f726c64", // 1234 + "hello world"
-		FrameRef{BlockHeight: 1234, TxCommitment: []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64}},
+			"valid frame reference",
+			"d20400000000000068656c6c6f20776f726c64", // 1234 + "hello world"
+			FrameRef{BlockHeight: 1234, TxCommitment: []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64}},
+			true,
+			nil,
+		},
+		{
+			"invalid frame reference",
+			"4269",
+			FrameRef{},
+			false,
+			ErrInvalidSize,
 		},
 	}
 	for _, tt := range tests {
@@ -24,7 +35,12 @@ func TestEncodeDecodeFrameRef(t *testing.T) {
 			frameRef, err := hex.DecodeString(tt.frameRefHex)
 			require.NoError(t, err)
 			gotFrameRef := FrameRef{}
-			gotFrameRef.UnmarshalBinary(frameRef)
+			err = gotFrameRef.UnmarshalBinary(frameRef)
+			if !tt.isValid {
+				require.ErrorIs(t, err, ErrInvalidSize)
+				return
+			}
+			require.NoError(t, err)
 			require.Equal(t, tt.frameRef, gotFrameRef)
 			frameRefHex, err := tt.frameRef.MarshalBinary()
 			require.NoError(t, err)
