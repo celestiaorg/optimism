2603e9301106d9ffe233740b1aedb453e8af1ef0
Javed Khan <tuxcanfly@gmail.com>
2023-11-02 23:36:21 -0700

    fix: testnet-up.sh - da container name

--- ops-bedrock/testnet-up.sh
+++ ops-bedrock/testnet-up.sh
@@ -103,7 +103,7 @@ L2OO_ADDRESS="0x6900000000000000000000000000000000000000"
   echo "Bringing up devnet..."
   docker-compose -f docker-compose-testnet.yml up -d da
   wait_up http://localhost:26659/header/1
-  export CELESTIA_NODE_AUTH_TOKEN="$(docker exec ops-bedrock_da_1 celestia light auth admin --node.store /home/celestia/.celestia-light-mocha-4/)"
+  export CELESTIA_NODE_AUTH_TOKEN="$(docker exec ops-bedrock-da-1 celestia light auth admin --node.store /home/celestia/.celestia-light-mocha-4/)"
   echo "CELESTIA_NODE_AUTH_TOKEN=$CELESTIA_NODE_AUTH_TOKEN"
   L2OO_ADDRESS="$L2OO_ADDRESS" \
       docker-compose -f docker-compose-testnet.yml up -d op-proposer op-batcher
