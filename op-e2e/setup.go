697cc51ed4620d118a659fa22d4f35487efaccc1
Javed Khan <tuxcanfly@gmail.com>
2024-03-25 15:45:17 -0700

    da: upgrade to go-da v0.5.0

--- op-e2e/setup.go
+++ op-e2e/setup.go
@@ -144,7 +144,7 @@ func DefaultSystemConfig(t testing.TB) SystemConfig {
 				RuntimeConfigReloadInterval: time.Minute * 10,
 				ConfigPersistence:           &rollupNode.DisabledConfigPersistence{},
 				Sync:                        sync.Config{SyncMode: sync.CLSync},
-				DaConfig:                    celestia.Config{DaRpc: "localhost:26650"},
+				DaConfig:                    celestia.CLIConfig{Rpc: "grpc://localhost:26650"},
 			},
 			RoleVerif: {
 				Driver: driver.Config{
@@ -161,7 +161,7 @@ func DefaultSystemConfig(t testing.TB) SystemConfig {
 				RuntimeConfigReloadInterval: time.Minute * 10,
 				ConfigPersistence:           &rollupNode.DisabledConfigPersistence{},
 				Sync:                        sync.Config{SyncMode: sync.CLSync},
-				DaConfig:                    celestia.Config{DaRpc: "localhost:26650"},
+				DaConfig:                    celestia.CLIConfig{Rpc: "grpc://localhost:26650"},
 			},
 		},
 		Loggers: map[string]log.Logger{
